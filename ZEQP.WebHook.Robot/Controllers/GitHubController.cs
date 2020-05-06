16152f6ae07c8901609d55dac97e32a43f158491
刘巨 <liuju150@outlook.com>
2020-05-06 11:01:48 +0800

    增加GitHub

---
+++ ZEQP.WebHook.Robot/Controllers/GitHubController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace ZEQP.WebHook.Robot.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GitHubController : ControllerBase
+    {
+        public ILogger<GitHubController> Logger { get; set; }
+        public GitHubController(ILogger<GitHubController> logger)
+        {
+            this.Logger = logger;
+        }
+
+        [HttpPost]
+        public Task Post(object body)
+        {
+            var json = JsonConvert.SerializeObject(body);
+            this.Logger.LogInformation($"接收数据：{Environment.NewLine}{json}");
+            return Task.CompletedTask;
+        }
+    }
+}
